52e51f3d12a9016c5759a0aa3e1d0c6a815d8947
Juven T. Pinoy <juvenpinoy@gmail.com>
2026-04-29 15:40:23 +0800

    Fix dispatch save - set original value

--- P77DataMonitoringsite/settings.py
+++ P77DataMonitoringsite/settings.py
@@ -164,7 +164,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
+EMAIL_HOST = "smtp.sendgrid.net"
+EMAIL_PORT = 2525
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = config('EMAIL_HOST_USER')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
